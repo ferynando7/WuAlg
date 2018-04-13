c8ac5d3ff04589cf6f797223d8be66b24473bde1
Fernando Zhapa <ferynando7@gmail.com>
2018-04-13 01:36:12 -0500

    Nuevas funciones para sPolynomial'

--- src/Main.hs
+++ src/Main.hs
@@ -87,6 +87,61 @@ fullAscendentChain polys n = (getMinimalPoly polys n : fullAscendentChain (getPs
 
 
 
+--FUNCIONES NUEVAS--
+
+--Funciones que obtiene el maximo de cada posicion de dos arreglos
+maxDegrees :: [Int] -> [Int] -> [Int]
+maxDegrees [a] [b] = [max a b]
+maxDegrees (x:xs) (y:ys) = (max x y : maxDegrees xs ys)
+
+--Funcion que obtiene el lcmMonomial entre dos monomios expresados en arreglo de Int cada uno
+
+lcmMonomial' :: SNat n -> [Int] -> [Int] -> OrderedMonomial ord n
+lcmMonomial' n a b = 
+    let monomList = maxDegrees a b
+    in toMonomial n monomList 
+
+--Funcion que converte un arreglo de 
+toMonomial :: SNat n -> [Int] -> OrderedMonomial ord n
+toMonomial n a = orderMonomial Proxy (fromList n a)
+
+
+--Funcion que obtiene el leadingMonomial en terminos de arreglos de Int
+maxMonomial :: [[Int]] -> Int -> [Int]
+maxMonomial [a] _ = a
+maxMonomial (x:xs) n
+    | (x!!n) > (maxMonomial xs n)!!n = x
+    | otherwise = maxMonomial xs n
+
+--Funcion que obtiene el leadingMonomial de forma algebraica
+leadingMonomial' :: (IsOrderedPolynomial poly, Field (Coefficient poly))
+                    => poly -> SNat n -> Int -> OrderedMonomial ord n
+leadingMonomial' f n i = 
+    let a = map (V.toList) (HS.toList (monomials f))
+    in toMonomial n (maxMonomial a i) 
+
+
+--Nueva funcion sPolynomial en la cual se indica la variable con respecto a la cual se debe obtener el sPolynomial
+-- sPolynomial' :: (IsOrderedPolynomial poly, Field (Coefficient poly))
+--              => poly -> poly -> Int -> poly
+-- sPolynomial' f g n =
+--      let h = (one, lcmMonomial (leadingMonomial' f sONE n) (leadingMonomial' g sONE n))
+--      in toPolynomial (h tryDiv leadingTerm f) * f - toPolynomial (h tryDiv leadingTerm g) * g
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 --Algunos polinomios
 
 --Ideal 1
@@ -119,13 +174,40 @@ p5 =
 --
 
 
+monom1 = map (V.toList) (HS.toList (monomials p4))
+monom2 = map (V.toList) (HS.toList (monomials p5))
+
+maxM1 = maxMonomial monom1 1
+maxM2 = maxMonomial monom2 1
 
 
-result = fullAscendentChain [p4, p5] 0 
+leastCM = lcmMonomial' sONE maxM1 maxM2
 
+-- leadM1 = leadingMonomial' monom1 sONE 1
+-- leadM2 = leadingMonomial' monom2 sONE 1
+
+-- leadMON = lcmMonomial leadM1 leadM2
+
+
+ll1 = leadingMonomial' p1 sONE 0
+ll2 = leadingMonomial' p1 sONE 1
+ll3 = leadingMonomial' p1 sONE 2
+
+
+sONE :: SNat 3
+sONE = sing
 
 
 main :: IO()
 main = do
-    print result
-
+--    print monom1
+--    print monom2
+--    print maxM1
+--    print maxM2
+--    print leastCM
+--    print leadM1
+--    print leadM2
+--    print leadMON
+    print ll1
+    print ll2
+    print ll3
