d9fc9b0633136d80da9106eddf3ab5c2706ebf18
anthonyramos47 <anthonyramos47@gmail.com>
2018-07-20 11:09:01 -0500

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -65,7 +65,7 @@ y4= var 18
 z4= var 19
 
 
-q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20 :: Polynomial' 9
+q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20 :: Polynomial' 20
 -- First Cuadric
 ---------------------------------------------------
 -- grad x
