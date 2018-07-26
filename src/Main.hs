cfb1b36de1ccd284db8bfbf38808a1505632cbab
ferynando7 <ferynando7@gmail.com>
2018-07-25 22:26:24 -0500

    Primer commit con cálculo simbólico

--- src/Main.hs
+++ src/Main.hs
@@ -11,170 +11,34 @@
 
 module Main where
 import Algebra.Prelude                   hiding ((>>),(>>=))
-import Library.Wu
+import Simbolic.Wu
+import Simbolic.PolyClass
+import Simbolic.Expr
+import Simbolic.Mon
 
---ZONA DE SNATS
 sTwo :: SNat 2
 sTwo = sing
 
-sThree :: SNat 3
-sThree = sing
-
-sFour :: SNat 4
-sFour = sing
-
-sFive :: SNat 5
-sFive = sing
-
-sSix :: SNat 6
-sSix = sing
-
-sEight :: SNat 8
-sEight = sing
-
-sNine :: SNat 9
-sNine = sing
-
-
-sTwelve :: SNat 12
-sTwelve = sing
-
-sTwenty :: SNat 20
-sTwenty = sing
-
-sSixty :: SNat 60
-sSixty = sing
-
-
 x = var 0
-y= var 1
-z= var 2
-v= var 3
-l1= var 4
-l2= var 5
-l3= var 6
-l4= var 7
-x1= var 8
-y1= var 9
-z1= var 10
-x2= var 11
-y2= var 12
-z2= var 13
-x3= var 14
-y3= var 15
-z3= var 16
-x4= var 17
-y4= var 18
-z4= var 19
-a = var 20
-b = var 21
-c = var  22
-d = var  23
-e = var 24
-f = var  25
-g = var  26
-h = var  27
-i = var  28
-j = var  29
-a2 = var 30
-b2 = var 31
-c2 = var 32
-d2 = var 33
-e2 = var 34
-f2 = var 35
-g2 = var 36
-h2 = var 37
-i2 = var 38
-j2 = var 39
-a3 = var 40
-b3 = var 41
-c3 = var 42
-d3 = var 43
-e3 = var 44
-f3 = var 45
-g3 = var 46
-h3 = var 47
-i3 = var 48
-j3 = var 49
-a4 = var 50
-b4 = var 51
-c4 = var 52
-d4 = var 53
-e4 = var 54
-f4 = var 55
-g4 = var 56
-h4 = var 57
-i4 = var 58
-j4 = var 59
+y = var 1
 
+a :: Expr Integer
+a = Const 1
+-- aa :: OrderedMonomial order n
+-- aa = x
 
+-- xx :: PolynomialSym 2
+-- xx = (Const 1) * x
+aa = 2
 
+q1,q2, q3 :: PolynomialSym 2
+q1 = y^2 - x^2 -x^3
+q2 = y^2 + x^2 - 1
+q3 = x
+-- charSet = ascendentChain [q1,q2] [] [] sTwo 0
 
-
-
-
-
-
-
-
-q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20 :: Polynomial' 60
--- First Cuadric
----------------------------------------------------
--- grad x
-q1 = x - x1 - l1*(2 * a* x1 + d*
-y1 + e*z1 + g )
--- grad y
-q2 = y - y1 - l1*(2 * b* y1 + d*x1 + f *z1 + h )
--- grad z
-q3 = z - z1 - l1*(2 * c* z1 + e*x1 + f *z1 + i )
--- distance
-q4 = (x-x1)^2 + (y-y1)^2 + (z-z1)^2 - v^2
--- Quadic
-q5 = a * x1^2 + b*y1^2 + c*z1^2 + d*x1*y1 + e*x1*z1 + f*y1 *z1 + g*x1 + h*y1 + i *z1 + j
----------------------------------------------------
--- Second Quadric
----------------------------------------------------
--- grad x
-q6= x - x2 - l2*(2 * a2* x2 + d2*y2 + e2 *z2 + g2 )
--- grad y
-q7 = y - y2 - l2*(2 * b2* y2 + d2*x2 + f2 *z2 + h2 )
--- grad z
-q8 = z - z2 - l2*(2 * c2* z2 + e3*x2 + f2 *z2 + i2 )
--- distance
-q9 = (x-x2)^2 + (y-y2)^2 + (z-z2)^2 - v^2
--- Quadic
-q10 =  a2 * x2^2 +  b2 *y2^2 + c2 *z2^2 + d2*x2*y2 + e2*x2*z2 + f2 *y2 *z2 + g2*x2 + h2*y2 + i *z2 + j2
----------------------------------------------------
--- Third Quadric
----------------------------------------------------
--- grad x
-q11 = x - x3 - l3*(2 *a3* x3 + d3*y3 + e3 *z3 + g3 )
--- grad y
-q12 = y - y3 - l3*(2 *b3* y3 + d3*x3 + f3 *z3 + h3 )
--- grad z
-q13 = z - z3 - l3*(2 *c3* z3 + e3*x3 + f3 *z3 + i3 )
--- distance
-q14 = (x-x3)^2 + (y-y3)^2 + (z-z3)^2 - v^2
--- Quadic
-q15 =  a3 * x3^2 + b3*y3^2 + c3*z3^2 + d3*x3*y3 + e3*x3*z3 + f3*y3 *z3 + g3*x3 + h3*y3 + i3 *z3 + j3
----------------------------------------------------
----------------------------------------------------
--- Fourth Quadric
--- grad x
-q16 = x - x4 - l4*(2 * a4* x4 + d4*y4 + e4 *z4 + g4 )
--- grad y
-q17 = y - y4 - l4*(2 * b4* y4 + d4*x4 + f4 *z4 + h4 )
--- grad z
-q18 = z - z4 - l4*(2 * c4* z4 + e4*x4 + f4 *z4 + i4 )
--- distance
-q19 = (x-x4)^2 + (y-y4)^2 + (z-z4)^2 - v^2
--- Quadic
-q20 = a4* x4^2 + b4*y4^2 + c4*z4^2 + d4*x4*y4 + e4*x4*z4 + f4*y4 *z4 + g4*x4 + h4*y4 + i4 *z4 + f4
----------------------------------------------------
-
-charSet = ascendentChainWithConstants [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20] [] [] sSixty 0 20
 
 main :: IO()
 main = do
       putStrLn "\n Characteristic SET"
-      print charSet
+--      print charSet
---
+++ src/Simbolic/Wu.hs
@@ -0,0 +1,118 @@
+{-# LANGUAGE MultiParamTypeClasses, NoImplicitPrelude              #-}
+
+module Simbolic.Wu
+(
+    ascendentChain,
+    ascendentChainWithConstants
+) where
+
+import Algebra.Prelude
+import Simbolic.Mon
+import Simbolic.PolyClass
+
+
+
+pseudoRemainder :: (IsOrder n order, KnownNat n, Eq k, Num k, IsMonomialOrder n order, Euclidean k, Integral k)
+                    => SNat n -> Int -> OrderedPolynomial k order n -> OrderedPolynomial k order n -> OrderedPolynomial k order n
+pseudoRemainder sN var g f
+                | classVarDeg f sN var < classVarDeg g sN var || classVarDeg g sN var == 0 = f
+                | otherwise = pseudoRemainder  sN var g (sPolynomial' f g sN var)
+
+------------------FUNCION QUE OBTIENE LOS PSEUDOREMAINDERS DE UN CONJUNTO DE POLINOMIOS
+getPseudoRemainders :: (IsOrder n order, KnownNat n, Eq k, Num k, Ord k, IsMonomialOrder n order, Euclidean k, Integral k)
+        => [OrderedPolynomial k order n] -> SNat n -> Int -> [OrderedPolynomial k order n]
+getPseudoRemainders pols nat var = map (pseudoRemainder nat var divisor) dividens
+        where 
+                divisor = minimalPolyWithVar pols nat var
+                dividens = dividendPolys pols nat var
+
+invPseudoRemainders :: (IsOrder n order, KnownNat n, Eq k, Num k, IsMonomialOrder n order, Euclidean k, Integral k)
+        => [OrderedPolynomial k order n] -> OrderedPolynomial k order n -> SNat n -> Int -> [OrderedPolynomial k order n]
+invPseudoRemainders pols pol nat var = map (foo nat var pol) pols
+        where
+                foo = \sN vari g f -> pseudoRemainder sN vari f g
+        
+
+---------FUNCION QUE OBTIENE LA CADENA ASCENDENTE (Intento fallido...)-------
+
+ascChain :: (IsOrder n order, KnownNat n, Eq k, Num k, Ord k, IsMonomialOrder n order, Euclidean k, Integral k)
+        => [OrderedPolynomial k order n] -> [OrderedPolynomial k order n] -> SNat n -> Int -> [OrderedPolynomial k order n]
+ascChain [] chain _ _ = chain
+ascChain [a] chain nat var = getNewChain chain a nat var
+ascChain ideal [] nat var = ascChain newIdeal newChain nat (var+1)
+        where
+                newIdeal = getNewIdeal pseudos divisor minPoly nat var
+                newChain = getNewChain [] divisor nat var
+                pseudos = map (pseudoRemainder nat var minPoly) dividends
+                minPoly = minimalPolyWithVar ideal nat var
+                dividends = dividendPolys ideal nat var
+                divisor = minimalPolyWithVar (minPoly:pseudos) nat var
+ascChain ideal chain nat var = ascChain newIdeal newChain nat (var+1)
+
+        where
+                minPoly = minimalPolyWithVar ideal nat var
+                dividends = dividendPolys ideal nat var
+                pseudos = map (pseudoRemainder nat var minPoly) dividends
+                divisor = minimalPolyWithVar (pseudos) nat var
+                newChain = getNewChain chain divisor nat var
+                newIdeal = getNewIdeal pseudos divisor (newChain!!0) nat var
+
+getNewIdeal :: (IsOrder n order, KnownNat n, Eq k, Num k, Ord k, IsMonomialOrder n order, Euclidean k, Integral k)
+        => [OrderedPolynomial k order n] -> OrderedPolynomial k order n -> OrderedPolynomial k order n -> SNat n -> Int -> [OrderedPolynomial k order n]
+getNewIdeal oldIdeal divisor elemChain nat var = map (\a -> if a == divisor && divisor /= elemChain then pseudoRemainder nat var divisor elemChain else a) oldIdeal 
+        
+--chain, divisor
+getNewChain :: (IsOrder n order, KnownNat n, Eq k, Num k, Ord k, IsMonomialOrder n order, Euclidean k, Integral k)
+        => [OrderedPolynomial k order n] ->  OrderedPolynomial k order n -> SNat n -> Int -> [OrderedPolynomial k order n]
+getNewChain [] divisor nat var = [divisor]
+getNewChain chain divisor nat var = (minInvs:invPseudos) 
+        where
+                invPseudos = invPseudoRemainders chain divisor nat var
+                minInvs = minimalPolyWithVar (invPseudos) nat var
+
+----------------------------- FIN DE INTENTO FALLIDO
+
+
+
+
+
+
+ascendentChain :: (IsOrder n order, KnownNat n, Eq k, Num k, Ord k, IsMonomialOrder n order, Euclidean k, Integral k)
+        => [OrderedPolynomial k order n] -> [OrderedPolynomial k order n]  ->  [OrderedPolynomial k order n] -> SNat n -> Int -> [OrderedPolynomial k order n]
+-- La funcion necesita una condicion de parada P que debe ser igual al numero de variables de los polinomios
+ascendentChain polys [a] _ sN var = [a]
+ascendentChain polys [] [] sN var =  (possiblePoly: ascendentChain polys pseudos [possiblePoly] sN (var+1))
+        where  
+                minPoly = minimalPolyWithVar polys sN var
+                possiblePoly = minimalPolyWithVar (minPoly : (getPseudoRemainders polys sN var) ) sN var
+                pseudos = map (\p -> if p == possiblePoly && minPoly /= possiblePoly then pseudoRemainder sN var possiblePoly minPoly else p) (getPseudoRemainders polys sN var)
+ascendentChain polys pseudos oldChain sN var =  (checkChainPoly : ascendentChain polys pseudos1 (oldChain ++ [checkChainPoly]) sN (var+1) )
+                -- En caso de que var == p entonces paramos la funcion
+        where   
+                checkChainPoly = minimalPolyWithVar (invPseudoRemainders oldChain possiblePoly sN var) sN var
+                possiblePoly = minimalPolyWithVar pseudos sN var
+                pseudos1 = map (\p -> if p == possiblePoly && checkChainPoly /= possiblePoly then pseudoRemainder sN var possiblePoly checkChainPoly else p) (getPseudoRemainders pseudos sN var)                
+
+
+ascendentChainWithConstants :: (IsOrder n order, KnownNat n, Eq k, Num k, Ord k, IsMonomialOrder n order, Euclidean k, Integral k)
+        => [OrderedPolynomial k order n] -> [OrderedPolynomial k order n]  ->  [OrderedPolynomial k order n] -> SNat n -> Int -> Int -> [OrderedPolynomial k order n]
+        -- La funcion necesita una condicion de parada P que debe ser igual al numero de variables de los polinomios
+ascendentChainWithConstants polys [a] _ sN var lim = [a]
+ascendentChainWithConstants _ _ _ _ _ 0 = []
+ascendentChainWithConstants polys [] [] sN var lim =  (possiblePoly: ascendentChainWithConstants polys pseudos [possiblePoly] sN (var+1) (lim-1))
+        where  
+                minPoly = minimalPolyWithVar polys sN var
+                possiblePoly = minimalPolyWithVar (minPoly : (getPseudoRemainders polys sN var) ) sN var
+                pseudos = map (\p -> if p == possiblePoly && minPoly /= possiblePoly then pseudoRemainder sN var possiblePoly minPoly else p) (getPseudoRemainders polys sN var)
+ascendentChainWithConstants polys pseudos oldChain sN var lim =  (checkChainPoly : ascendentChainWithConstants polys pseudos1 (oldChain ++ [checkChainPoly]) sN (var+1) (lim-1))
+        -- En caso de que var == p entonces paramos la funcion
+        where   
+                checkChainPoly = minimalPolyWithVar (invPseudoRemainders oldChain possiblePoly sN var) sN var
+                possiblePoly = minimalPolyWithVar pseudos sN var
+                pseudos1 = map (\p -> if p == possiblePoly && checkChainPoly /= possiblePoly then pseudoRemainder sN var possiblePoly checkChainPoly else p) (getPseudoRemainders pseudos sN var)                
+
+
+
+
+
+
